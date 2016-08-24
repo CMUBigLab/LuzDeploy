ff49e870e8db4b80b0b315e28c1435e544c4c25e
Cole Gleason <cole@colegleason.com>
2016-08-23 22:40:54 -0400

    fix

--- consent.js
+++ consent.js
@@ -3,7 +3,7 @@ const Consent = require('./models/consent')
 const handlers = require('./handlers')
 
 module.exports.post = function(req, res) {
-	new Consent({fbid: req.body.id, date: new Date()}).save().then(() => {
+	new Consent({fbid: req.body.fbid, date: new Date()}).save().then(() => {
 		res.send('Thanks! Please press the back button.')
 		handlers.sendDeploymentMessage(fbid)
 	})
