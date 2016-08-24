36fe8ecc07aa786bea113392ef0d40183c6cce12
Cole Gleason <cole@colegleason.com>
2016-08-23 22:44:21 -0400

    debug

--- consent.js
+++ consent.js
@@ -3,7 +3,8 @@ const Consent = require('./models/consent')
 const handlers = require('./handlers')
 
 module.exports.post = function(req, res) {
-	new Consent({fbid: req.body.fbid, date: new Date()}).save().then(() => {
+	console.log(req.body.fbid)
+	new Consent().save({fbid: req.body.fbid, date: new Date()}).then(() => {
 		res.send('Thanks! Please press the back button.')
 		handlers.sendDeploymentMessage(fbid)
 	})
