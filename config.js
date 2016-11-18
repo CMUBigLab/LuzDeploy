691940c760b0a9128a267562734506f9f0f60f84
Cole Gleason <cole@colegleason.com>
2016-11-18 11:58:49 -0500

    pass next uri along

--- config.js
+++ config.js
@@ -5,11 +5,15 @@ if (!process.env.HEROKU_APP_NAME) {
 	throw new Error("HEROKU_APP_NAME is required.")
 }
 
+var threadId = process.env.HEROKU_APP_NAME == 'luzdeploy-staging' ?
+	'1146606925407192': '720072294762496';
+
 module.exports = {
 	DB_CONFIG: {
 		client: 'pg',
 		connection: process.env.DATABASE_URL,
 		debug: process.env.DEBUG
 	},
-	BASE_URL: `http://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+	BASE_URL: `http://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
+	THREAD_URI: `fb-messenger://user/${threadId}`
 }
--- controllers/sweep_task.js
+++ controllers/sweep_task.js
@@ -14,7 +14,7 @@ var SweepTaskFsm = machina.BehavioralFsm.extend({
 				var buttons = [{
  					"type":"web_url", 
  					"title": "Open NavCog", 
- 					"url": `http://hulop.qolt.cs.cmu.edu/?type=beaconsweeper&major=65535&edge=${params.edge}&beaconlist=${params.beacons}&wid=${task.get('volunteer_fbid')}&start=${params.start}&end=${params.end}`
+ 					"url": `http://hulop.qolt.cs.cmu.edu/?type=beaconsweeper&major=65535&edge=${params.edge}&beaconlist=${params.beacons}&wid=${task.get('volunteer_fbid')}&start=${params.start}&end=${params.end}&next=${config.THREAD_URI}`
  				}];
 				bot.sendMessage(
 					task.get('volunteer_fbid'),
