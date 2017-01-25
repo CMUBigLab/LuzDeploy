9385cbddf9ca9099f1162eb80bf9d64d03e6c67a
Cole Gleason <cole@colegleason.com>
2017-01-25 15:25:16 -0500

    add script to create sweep tasks

---
+++ scripts/create_sweep_tasks.js
@@ -0,0 +1,39 @@
+var BeaconSlot = require('../models/beacon-slot');
+var Task = require('../models/task');
+
+var Promise = require('bluebird');
+
+// adapted from https://stackoverflow.com/questions/2270910
+function getRanges(array) {
+  var ranges = [], rstart, rend;
+  for (var i = 0; i < array.length; i++) {
+    rstart = array[i];
+    rend = rstart;
+    while (array[i + 1] - array[i] == 1) {
+      rend = array[i + 1]; // increment the index if the numbers sequential
+      i++;
+    }
+    ranges.push(rstart == rend ? rstart.toString() : rstart + '-' + rend);
+  }
+  return ranges.join();
+}
+
+BeaconSlot.collection()
+.query('where','beacon_id', 'is not', null)
+.fetch()
+.then(function(slots) {
+	var edges = slots.groupBy('edge');
+	return Promise.map(Object.keys(edges), function(edge) {
+		var beaconSlots = edges[edge];
+		return Task.forge({
+			deploymentId: 1,
+			templateType: 'sweep_edge',
+			instructionParams: {
+				edge: edge,
+				start: beaconSlots[0].get('startNode'),
+				end: beaconSlots[0].get('endNode'),
+				beacons: getRanges(beaconSlots.map(b => b.id).sort()),
+			}
+		}).save();
+	})
+});
