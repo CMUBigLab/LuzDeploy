781bb1f90ef5a7a85f641534d587768d08aa9346
Cole Gleason <cole@colegleason.com>
2017-01-26 14:32:16 -0500

    debug

--- models/deployment.js
+++ models/deployment.js
@@ -27,6 +27,7 @@ const Deployment = bookshelf.model('BaseModel').extend({
 		.query({where:{completed: false, volunteer_fbid: null, disabled: false}})
 		.fetch()
 		.tap(pool => pool.sortBy(['template_type', function(task) {
+			console.log('task type', task.get('templateType'));
 			if (task.get('templateType') == 'sweep_edge') {
 				console.log(task.get('instructionParams'), task.get('instructionParams').edge);
 				return task.get('instructionParams').edge;
