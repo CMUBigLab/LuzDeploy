5136ac3f41033a90e61c384e470bf48085c3224d
Cole Gleason <cole@colegleason.com>
2017-02-01 16:32:18 -0500

    debug

--- routes.js
+++ routes.js
@@ -142,6 +142,7 @@ router.post('/send-message', bodyParser.json(), function(req, res, next) {
 	} else if (buttons) {
 		message = msgUtils.buttonMessage(text, buttons);
 	}
+	console.log(req.body);
 	console.log(message, text, buttons);
 	if (mass === true) {
 		Deployment.forge({id: deploymentId}).volunteers()
