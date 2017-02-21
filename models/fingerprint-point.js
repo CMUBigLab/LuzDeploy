822220f15d3d3804231fb15f8ec990909829df61
Cole Gleason <cole@colegleason.com>
2017-02-21 17:26:05 -0500

    use fetchAll instead of collection().query()

--- models/fingerprint-point.js
+++ models/fingerprint-point.js
@@ -12,7 +12,7 @@ const FingerprintPoint = bookshelf.model('BaseModel').extend({
 }, {
 	getPointsForSampling: function(deploymentId, limit) {
 		limit = limit || 1;
-		this.collection().query({withRelated: ['sample']})
+		this.fetchAll({withRelated: ['sample']})
 		.then(function(points) {
 			return points.sortBy(function(p) {
 				return p.related('samples').length;
