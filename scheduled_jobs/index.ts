a413450b2ec99c04149c4e86e40f9e88da90c8bc
Cole Gleason <cole@colegleason.com>
2017-03-07 14:04:51 -0500

    change time for test

--- scheduled_jobs/index.ts
+++ scheduled_jobs/index.ts
@@ -10,8 +10,8 @@ const jobSchedule = [{
     name: "remind users of new tasks",
     function: remindVolunteersOfTasksAvailable,
     weekdays: [1, 2, 3, 4, 5],
-    startTime: "14:19",
-    endTime: "14:25",
+    startTime: "14:09",
+    endTime: "14:15",
 }];
 
 const now = moment().tz(TIME_ZONE);
