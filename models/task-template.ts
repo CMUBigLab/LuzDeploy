62c43dbac0d4991fe3f2118ca4617a6d004dc2cb
Cole Gleason <cole@colegleason.com>
2017-03-07 14:46:48 -0500

    more debugging

--- models/task-template.ts
+++ models/task-template.ts
@@ -30,7 +30,7 @@ export class TaskTemplate extends bookshelf.Model<TaskTemplate> {
 
   get estimatedTimeMin(): number {
     const int = _.defaults(this.estimatedTime, {hours: 0, minutes: 0, seconds: 0});
-    console.log(this.estimatedTime, int, int.hours * 60 + int.minutes + int.seconds / 60);
+    console.log(this, this.estimatedTime, int, int.hours * 60 + int.minutes + int.seconds / 60);
     return int.hours * 60 + int.minutes + int.seconds / 60;
     }
 
