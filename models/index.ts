a93cd43d8a40a28334d3c04aaef3a4690b191946
Cole Gleason <cole@colegleason.com>
2017-03-09 16:41:54 -0500

    export all models from models/index.ts

---
+++ models/index.ts
@@ -0,0 +1,9 @@
+export { Admin } from "./admin";
+export { BeaconSlot } from "./beacon-slot";
+export { Beacon } from "./beacon";
+export { Deployment } from "./deployment";
+export { FingerprintPoint } from "./fingerprint-point";
+export { FingerprintSample } from "./fingerprint-sample";
+export { TaskTemplate } from "./task-template";
+export { Task } from "./task";
+export { Volunteer } from "./volunteer";
