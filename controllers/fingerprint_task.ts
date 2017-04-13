49a517a79dbf449060421b7d7dc407d605a637ff
Cole Gleason <cole@colegleason.com>
2017-04-13 13:07:21 -0400

    10 -> 2 fingerprint points

--- controllers/fingerprint_task.ts
+++ controllers/fingerprint_task.ts
@@ -22,7 +22,7 @@ export const FingerprintTaskFsm = machina.BehavioralFsm.extend({
                 let self = this;
                 FingerprintPoint.getPointsForSampling(
                     task.deploymentId,
-                    10
+                    2
                 ).then(function(points) {
                     task.context = {
                         points: points.map((p: FingerprintPoint) => ({
