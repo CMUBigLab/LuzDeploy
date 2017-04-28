6baa9d1b21b0861bf94a72978e33ffea887fa75c
Cole Gleason <cole@colegleason.com>
2017-04-28 17:04:44 -0400

    more logging for debug

--- models/volunteer.ts
+++ models/volunteer.ts
@@ -79,6 +79,7 @@ export class Volunteer extends BaseModel<Volunteer> {
         return this.save({current_task: null}, {patch: true});
     }
     unassignTask(): Promise<[Volunteer, Task]> {
+        console.log(this);
         return this.currentTask().fetch()
         .then((task: Task) => {
             return Promise.all([
