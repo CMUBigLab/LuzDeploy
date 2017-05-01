0b274fd2c4450a854cedcea85484a1d1fabf059b
Cole Gleason <cole@colegleason.com>
2017-05-01 12:48:48 -0400

    Change default fingerprint point amount to 5

--- controllers/fingerprint_task.ts
+++ controllers/fingerprint_task.ts
@@ -78,7 +78,7 @@ export const FingerprintTaskFsm = machina.BehavioralFsm.extend({
                 let self = this;
                 FingerprintPoint.getPointsForSampling(
                     task.deploymentId,
-                    2
+                    5
                 ).then(function(points) {
                     task.context = {
                         points: points.map((p: FingerprintPoint) => ({
