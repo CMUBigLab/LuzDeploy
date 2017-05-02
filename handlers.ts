2cef197bc1b0c0109f6e7f8d4fcac489218e3743
Cole Gleason <cole@colegleason.com>
2017-05-02 16:01:52 -0400

    debug

--- handlers.ts
+++ handlers.ts
@@ -586,7 +586,7 @@ function earnings(payload: WebhookPayloadFields, reply: ReplyFunc) {
     .fetch()
     .then(tasks => {
         const total = _.sum(tasks.map(t => t.compensation)) || 0;
-        console.log("total compensation", typeof tasks[0].compensation, total);
+        console.log("total compensation", typeof tasks.first().compensation, total);
         reply({text: `You have earned a total of $${total.toFixed(2)}. You can collect your earnings by contacting Cole Gleason (cgleason@cs.cmu.edu).`});
     })
 }
